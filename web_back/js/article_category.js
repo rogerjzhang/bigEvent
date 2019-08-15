04b36bc7bd304a44a064a411ec176564c0134f78
rogerjzhang <540900883@qq.com>
2019-08-15 18:06:15 +0800

    文章类别管理-删除按钮

--- web_back/js/article_category.js
+++ web_back/js/article_category.js
@@ -1,21 +1,86 @@
 window.onload = function () {
-    Vue.component('mylist', {
-        props:['data'],
-        template: '#tr_temp'
-    })
-    new Vue({
+
+    let vm = new Vue({
         el: '#article_category',
         data: {
-            categorylist:[]
+            categorylist: [],
+            name: '',
+            slug: '',
+            id: ''
+        },
+        methods: {
+            addType() {
+                this.$http.post('http://127.0.0.1:8080/admin/category/add', {
+                        name: this.name,
+                        slug: this.slug
+                    }, {
+                        emulateJSON: true
+                    })
+                    .then(res => {
+                        // console.log(res)
+                        if (res.body.code == 201) {
+                            $('#addModal').modal('hide')
+                            this.getList()
+                            this.name = ''
+                            this.slug = ''
+                        }
+                    }).catch(err => {
+                        // console.log(err)
+                        alert('分类名称或别名已存在');
+                        return
+                    })
+            },
+            getList() {
+                this.$http.get('http://127.0.0.1:8080/admin/category/list')
+                    .then((res) => {
+                        if (res.body.code == 200) {
+                            this.categorylist = res.body.data
+                        }
+                    })
+            },
+            del(id) {
+                console.log('123')
+                // if(confirm('确定要删除吗?')){
+                //     this.$http.post('http://127.0.0.1:8080/admin/category/delete',{id},{emulateJSON:true})
+                //     .then(res=>{
+                //         console.log(res)
+                //     })
+                // }
+            }
+
         },
         mounted() {
-            this.$http.get('http://127.0.0.1:8080/admin/category/list')
-                .then((res) => {
-                    console.log(res)
-                    if(res.body.code == 200){
-                        this.categorylist= res.body.data 
-                    }
-                })
+            this.getList()
+            // this.$http.get('http://127.0.0.1:8080/admin/category/list')
+            //     .then((res) => {
+            //         console.log(res)
+            //         if(res.body.code == 200){
+            //             this.categorylist= res.body.data 
+            //         }
+            //     })
         },
+        components: {
+            'mylist': {
+                props: ['data'],
+                template: '#tr_temp',
+                methods: {
+                    del(id) {
+                        if (confirm('确定要删除吗?')) {
+                            this.$http.post('http://127.0.0.1:8080/admin/category/delete', {
+                                    id
+                                }, {
+                                    emulateJSON: true
+                                })
+                                .then(res => {
+                                    vm.getList()
+                                })
+                        }
+                    }
+                }
+            }
+        }
+    })
+    $('#model_shutoff').on('click', function () {
+        $('#addModal').modal('hide')
     })
 }
